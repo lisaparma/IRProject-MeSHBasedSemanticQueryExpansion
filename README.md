d0188116bd7e93d77e3ec9fe0e122337b3bfd41d
Lisa Parma <lisa.parma@hotmail.it>
2020-01-11 15:26:42 +0100

    Setting for parsing docs and queries

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Project for Information Retrieval university course
 ## How to use it:
 - clone repository
 - insert terrier.project-5.1 directory into this repo
-- remane it as "terrier"
+- rename it as "terrier"
 - Run script.sh
 - Everything is ok :ok_hand:
--- script.sh
+++ script.sh
@@ -1,20 +1,45 @@
+#!/bin/bash
+
+DOCS="ohsumed.87" 
+QUERIES="query.ohsu.1-63"
+
+# Parse files
+echo "\n\n ------ PARSE FILES ------ \n"
+if [ -d "./parsedFiles" ];
+then
+	rm -rf parsedFiles
+fi
+mkdir parsedFiles
+
+# Parse Docs and Queries
+echo "\t --> Parsing ${DOCS} and moving it to parsedFiles/parsedDocs"
+python parseScript/parseDoc.py originalDocuments/${DOCS}
+mv originalDocuments/${DOCS}Parsed parsedFiles/parsedDocs
+
+echo "\t --> Parsing ${QUERIES} and moving it to parsedFiles/parsedQueries"
+python parseScript/parseQuery.py originalDocuments/${QUERIES}
+mv originalDocuments/${QUERIES}Parsed parsedFiles/parsedQueries
+
 
 # Set files to index
 echo "\n\n ------ SET FILES ------ \n"
-sh terrier/bin/trec_setup.sh test
+sh terrier/bin/trec_setup.sh parsedFiles/parsedDocs
 
-# overwriting current properties with mine
+
+# Overwriting current properties with mine
 mv terrier/etc/terrier.properties terrier/etc/terrier.properties.bak
 cp terrier.properties terrier/etc/terrier.properties
 
-# Removed index dir
+# Remove index dir and create a new one
 if [ -d "./terrier/var/index" ];
 then
     rm -rf terrier/var/index
     mkdir terrier/var/index
+else 
+	mkdir terrier/var/index
 fi
 
-# indexing of collection
+# Indexing of collection
 echo "\n\n ------ INDEX CREATION ------ \n"
 sh terrier/bin/terrier batchindexing
 
@@ -22,5 +47,16 @@ sh terrier/bin/terrier batchindexing
 echo "\n\n ------ INDEX STATISTICS ------ \n"
 sh terrier/bin/terrier indexstats
 
-# Test it with intecatitive mode
-# bin/terrier interactive
+# Indexing stats
+echo "\n\n ------ RETRIVAL ------ \n"
+sh terrier/bin/terrier batchretrieve \
+	-w BM25 \
+	-t parsedFiles/parsedQueries
+
+
+# TODO
+#echo "\n\n ------ EVALUATE ------ \n"
+#sh terrier/bin/terrier batchevaluate -q qrels.ohsu.batch.87
+
+
+echo "\n"
