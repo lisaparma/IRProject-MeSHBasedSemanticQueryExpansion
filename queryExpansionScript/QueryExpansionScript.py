0179eb63f410eb59eaa344cd2364572304a40d4d
Lisa Parma <lisa.parma@hotmail.it>
2020-01-24 12:58:55 +0100

    Expanded QueryExpansionScript

--- queryExpansionScript/QueryExpansionScript.py
+++ queryExpansionScript/QueryExpansionScript.py
@@ -1,13 +1,13 @@
 import sys
-from whoosh import qparser
+
 from whoosh.analysis import RegexTokenizer
 from whoosh.lang.porter import stem
-from whoosh.lang.morph_en import variations
 from whoosh.analysis import StopFilter
 
 from MeSH import MeSH
 
 data = MeSH("desc2020.xml").map
+
 
 def findPreferredTerm(concept):
     for term in concept["termlist"]:
@@ -24,12 +24,12 @@ def findPreferredConcept(descriptor):
     print("ERROR: preferred concept in " + descriptor["name"] + " not found !!!")
     return None
 
+
 def mesh(word):
     termsToAdd = []      # Array to fill with terms to add to query (point 3)
     conceptsToAdd = []   # Array to fill with concepts to add to query (point 4)
 
     for descriptor in data.keys():
-
         findConcept = False
         for concept in data[descriptor]:
             findTerm = False
@@ -40,35 +40,26 @@ def mesh(word):
 
             if findTerm == True:
                 prefTerm = findPreferredTerm(data[descriptor][concept])
-                for part in prefTerm.replace(",", "").split(" "):
+                for part in prefTerm.split(", "):
                     termsToAdd.append(part)
                                      
                 # word is also in concept name ?
                 if word in data[descriptor][concept]["name"]:
                     findConcept = True
-                    
 
         if findConcept == True:
             prefConcept = findPreferredConcept(data[descriptor])
-            for part in prefConcept.replace(",", "").split(" "):
+            for part in prefConcept.split(", "):
                 conceptsToAdd.append(part)
-    '''
-    print("--> " + word)
-    print("TERMS:")
-    for term in termsToAdd:
-        print("\t" + term)
 
-    print("CONCEPTS:")
-    for concept in conceptsToAdd:
-        print("\t" + concept)
-    '''
     return (termsToAdd, conceptsToAdd)
 
-def queryindex(query):
+
+def queryIndex(query):
     tokenizer = RegexTokenizer()
     return_list = []   
     
-    #Removing stop words
+    # Removing stop words
     with open("../smartStopList.txt", "r") as fp:
         line = fp.readline()
         words=[]
@@ -80,56 +71,68 @@ def queryindex(query):
     tokens = stopper(tokenizer(query))
 
     for t in tokens:
-        #converting to lower case
-        t.text = t.text.lower()
-        #stemming
-        s=stem(t.text)
-        return_list.append(s)
+        t.text = t.text.lower()  # Converting to lower case
+        s = stem(t.text)  # stemming
+        if len(s) > 2:
+            return_list.append(s)
     return return_list
 
+
 def doExpansion(query):
-    words = queryindex(query)
+    words = queryIndex(query)
     terms = []
     concepts = []
     for word in words:
         termsToAdd, conceptsToAdd = mesh(word)
         terms = terms + termsToAdd
         concepts = concepts + conceptsToAdd
-    return ' '.join(list(set(terms)))
 
+    listRun3 = ' '.join(list(set(terms)))
+    listRun4 = ' '.join(list(set(concepts)))
+    listRun5 = ' '.join(list(set(terms + concepts)))
+    return (listRun3, listRun4, listRun5)
 
 def main():
-    print("\n\n")
-    newFile = open("queryExpansioned","w")
+    # Create 3 different file with queries expanded in three different way
+    QErun3 = open("QueriesExp-run3", "w")
+    QErun4 = open("QueriesExp-run4", "w")
+    QErun5 = open("QueriesExp-run5", "w")
+
     with open("testquery", "r") as fp:
+
         line = fp.readline()
         while line:
-            if("<num>" in line):
-                newFile.write(line)
-                line = fp.readline()
-            if("</title>" in line):
-                newFile.write(line[:-9])
-                print("--> ESPANSIONE TITOLO")
-                print(line[8:-9])
+            if "</title>" in line:
+                QErun3.write(line[:-9])
+                QErun4.write(line[:-9])
+                QErun5.write(line[:-9])
+
                 expansion = doExpansion(line[8:-9])
-                print(expansion)
-                newFile.write("\n" + expansion + line[-9:])
+                QErun3.write("\n" + expansion[0] + line[-9:])
+                QErun4.write("\n" + expansion[1] + line[-9:])
+                QErun5.write("\n" + expansion[2] + line[-9:])
+
                 line = fp.readline()
-            if("</desc>" in line):
-                newFile.write(line[:-8])
-                print("\n--> ESPANSIONE DESCRIZIONE")
-                print(line[:-8])
+            if "</desc>" in line:
+                QErun3.write(line[:-8])
+                QErun4.write(line[:-8])
+                QErun5.write(line[:-8])
+
                 expansion = doExpansion(line[:-8])
-                print(expansion)
-                newFile.write("\n" + expansion + line[-8:])
+                QErun3.write("\n" + expansion[0] + line[-8:])
+                QErun4.write("\n" + expansion[1] + line[-8:])
+                QErun5.write("\n" + expansion[2] + line[-8:])
+
                 line = fp.readline()
             else:
-                newFile.write(line)
+                QErun3.write(line)
+                QErun4.write(line)
+                QErun5.write(line)
                 line = fp.readline()
-    newFile.close()
-    print("\n\n")
+    QErun3.close()
+    QErun4.close()
+    QErun5.close()
   
     
-if __name__=="__main__": 
-    main() 
-    
+if __name__== "__main__":
+    main()
